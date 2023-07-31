720d044f412b9564c3a7e09b17a00716ff193083
Flippie Coetser <flippie.coetser@gmail.com>
2023-07-31 08:13:54 -0700

    CODE RUB: Align Spacing and Indentation

--- R/Todo.Broker.R
+++ R/Todo.Broker.R
@@ -7,23 +7,19 @@
 Todo.Broker <- \(storage){
   operations <- list()
 
-  operations[['Insert']] <- \(todo){
+  operations[['Insert']]     <- \(todo){
     todo |> storage[['Todo']][['Insert']]()
   }
-
-  operations[['Select']] <- \(...){
+  operations[['Select']]     <- \(...){
     ... |> storage[['Todo']][['Select']]()   
   }
-
   operations[['SelectById']] <- \(id){
     id |> storage[['Todo']][['SelectWhereId']]()
   }
-
-  operations[['Update']] <- \(todo){
+  operations[['Update']]     <- \(todo){
     todo |> storage[['Todo']][['Update']]()
   }
-
-  operations[['Delete']] <- \(id){
+  operations[['Delete']]     <- \(id){
     id |> storage[['Todo']][['Delete']]()
   }
 
