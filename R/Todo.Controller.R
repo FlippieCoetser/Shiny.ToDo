f2e48390b941ca29e085063093a37bb4310efe50
Flippie Coetser <flippie.coetser@gmail.com>
2023-07-31 10:47:55 -0700

    CODE RUB: Update Naming Convention

--- R/Todo.Controller.R
+++ R/Todo.Controller.R
@@ -6,7 +6,7 @@ Todo.Controller <- \(id, data) {
       state <- reactiveValues()
       state[["todos"]] <- data[['Retrieve']]()
       state[["todo"]]  <- NULL
-      
+
      # Input Binding
       observeEvent(input[['create']], { coordinator[['create']](input[["newTask"]]) })
       observeEvent(input[["todos_rows_selected"]], { coordinator[["select"]](input[["todos_rows_selected"]]) }, ignoreNULL = FALSE )
@@ -14,14 +14,14 @@ Todo.Controller <- \(id, data) {
       observeEvent(input[["delete"]], { coordinator[["delete"]]() })
 
       # Input Verification
-      verification <- NULL
-      verification[["isTaskEmpty"]] <- reactive(input[["newTask"]] == '')
-      verification[["isTodoSelected"]] <- reactive(!is.null(input[["todos_rows_selected"]]))
+      verify <- list()
+      verify[["taskEmpty"]]    <- reactive(input[["newTask"]] == '')
+      verify[["todoSelected"]] <- reactive(!is.null(input[["todos_rows_selected"]]))
 
       # User Actions
       coordinator <- list()
       coordinator[['create']] <- \(task) {
-        if (!verification[["isTaskEmpty"]]()) {
+        if (!verify[["taskEmpty"]]()) {
           # Use the data layer to create a new todo
           task |> Todo.Model() |> data[['Add']]()
           # Use the data layer to update local state
@@ -31,7 +31,7 @@ Todo.Controller <- \(id, data) {
         }
       }
       coordinator[['select']] <- \(id) {
-        if (verification[["isTodoSelected"]]()) {
+        if (verify[["todoSelected"]]()) {
           state[["todo"]] <- state[["todos"]][id,]
 
           session |> updateTextInput("task", value = state[["todo"]][["Task"]])
