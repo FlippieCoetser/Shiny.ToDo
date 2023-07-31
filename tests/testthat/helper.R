50c73aa944541814a623cf58ff4e74f1e0ee83bf
Flippie Coetser <flippie.coetser@gmail.com>
2023-07-31 07:55:56 -0700

    TESTING: Add Expect.Equal Unit Test Helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,2 +1,3 @@
 expect.exist      <- \(component) component |> is.null() |> expect_equal(FALSE)
+expect.equal      <- \(actual, expected) actual |> expect_equal(expected)
 expect.list       <- \(members) members |> is.list() |> expect_equal(TRUE)
