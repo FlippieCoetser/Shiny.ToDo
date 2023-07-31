60e09eab8ffc0e37a6ebbd4e1f89febdc54e344b
Flippie Coetser <flippie.coetser@gmail.com>
2023-07-31 09:01:04 -0700

    VALIDATION: todo |> validate[['IsDuplicate']](todos) ThrowsExceptionIfTodoExistInTodos -> PASS

--- R/Todo.Logic.Validation.Service.R
+++ R/Todo.Logic.Validation.Service.R
@@ -1,5 +1,14 @@
 Todo.Logic.Validation.Service <- \() {
+  exception <- Todo.Exceptions()
+  
   validators <- list()
-  validators[["IsDuplicate"]] <- \() {}
+  validators[["IsDuplicate"]] <-  \(todo,todos) {
+    match.count <- 
+      todos |> dplyr::filter(Id == todo[["Id"]]) |> nrow()
+        
+    (match.count != 0) |> exception[["DuplicateKey"]]()
+
+    return(todo)
+  }
   return(validators)
 }
