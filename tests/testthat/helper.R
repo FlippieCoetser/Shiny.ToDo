7ffe745b7a9950e32b651d35ffce37ed5707b900
Flippie Coetser <flippie.coetser@gmail.com>
2023-07-31 08:10:55 -0700

    TESTING: Add Expect.Empty Unit Test Helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,4 @@
 expect.exist      <- \(component) component |> is.null() |> expect_equal(FALSE)
 expect.equal      <- \(actual, expected) actual |> expect_equal(expected)
 expect.list       <- \(members) members |> is.list() |> expect_equal(TRUE)
+expect.empty      <- \(entities) entities |> nrow() |> expect_equal(0)
