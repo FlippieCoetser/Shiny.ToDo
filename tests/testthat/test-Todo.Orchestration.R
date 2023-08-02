2ff17f5be5208b701a25db9063b6d5d6d544a650
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-02 03:53:14 -0700

    ORCHESTRATION: Todo.Orchestration() ReturnsListOfOrchestrations  -> FAIL

--- tests/testthat/test-Todo.Orchestration.R
+++ tests/testthat/test-Todo.Orchestration.R
@@ -4,3 +4,18 @@ describe('Given Todo.Orchestration',{
     Todo.Orchestration |> expect.exist()
   })
 })
+
+describe('When orchestrations <- storage |> Todo.Orchestration()',{
+  it('then operations is a list',{
+    # Given
+    configuration <- data.frame()
+
+    storage <- configuration |> Storage::Mock.Storage.Service()
+
+    # When
+    orchestrations <- storage |> Todo.Orchestration()
+
+    # Then
+    orchestrations |> expect.list()
+  })
+})
