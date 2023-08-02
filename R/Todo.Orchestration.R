2cee510612fb66b329f6e5968f59cd7ce91c5278
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-02 05:47:40 -0700

    ORCHESTRATION: todo |> orchestrate[['Add']]() ReturnsAllTodosIncludingTodo -> PASS

--- R/Todo.Orchestration.R
+++ R/Todo.Orchestration.R
@@ -1,5 +1,11 @@
 Todo.Orchestration <- \(storage) {
+  service <- storage |> Todo.Broker() |> Todo.Service()
   orchestrations <- list()
-  orchestrations[['Add']] <- \() {}
+  orchestrations[['Add']] <- \(todo) {
+    todo |> service[['Add']]()
+
+    todos <- service[['Retrieve']]()
+    return(todos)
+  }
   return(orchestrations)
 }
