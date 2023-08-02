55ddc9f5f29f32fb5fab0ea8a52837c4ca349578
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-02 11:47:36 -0700

    PROCESSING: Todo.Processing() ReturnsListContainingRetrieveProcessor -> FAIL

--- tests/testthat/test-Todo.Processing.R
+++ tests/testthat/test-Todo.Processing.R
@@ -18,4 +18,16 @@ describe('When processors <- storage |> Todo.Processing()',{
     # Then
     processors |> expect.list()
   })
+  it('then processors contains Retrieve processor',{
+    # Given
+    configuration <- data.frame()
+
+    storage <- configuration |> Storage::Mock.Storage.Service()
+
+    # When
+    processors <- storage |> Todo.Broker() |> Todo.Service() |> Todo.Processing()
+
+    # Then
+    processors[['Retrieve']] |> expect.exist()
+  })
 })
