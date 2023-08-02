7303cb4a95fcf495ec67868de31b9c28f167c615
Flippie Coetser <flippie.coetser@gmail.com>
2023-08-02 05:47:34 -0700

    ORCHESTRATION: todo |> orchestrate[['Add']]() ReturnsAllTodosIncludingTodo -> FAIL

--- tests/testthat/test-Todo.Orchestration.R
+++ tests/testthat/test-Todo.Orchestration.R
@@ -31,3 +31,25 @@ describe('When orchestrations <- storage |> Todo.Orchestration()',{
     orchestrations |> expect.list()
   })
 })
+
+describe('When todo |> orchestrate[["Add"]]()',{
+  it('then a data.frame with todos containing todo is returned',{
+    # Given
+    configuration <- data.frame()
+
+    storage <- configuration |> Storage::Mock.Storage.Service()
+
+    orchestrate <- storage |> Todo.Orchestration()
+
+    random.todo <- 'Task' |> Todo.Model()
+
+    new.todo      <- random.todo
+    expected.todo <- new.todo 
+
+    # When
+    retrieved.todos <- new.todo |> orchestrate[["Add"]]()
+
+    # Then
+    retrieved.todos |> expect.contain(expected.todo)
+  })
+})
