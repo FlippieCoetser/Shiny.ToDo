8cb92be2a0988b37a4cfc11ce889a124aae0009b
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-14 10:02:14 -0700

    CONTROLLER: Update to support Mock.Storage

--- server.R
+++ server.R
@@ -1,8 +1,14 @@
-# Mock Storage do not need configuration details
+# Mock Storage do not need configuration details but does require mock data
 # When using an Azure SQL Server, configuration details are needed
 # Configuration details can be stored in .Reviron file and retrieved via Environment Package
 configuration <- data.frame()
-storage       <- configuration |> Storage::Mock.Storage.Service()
+
+mock.data <- list()
+mock.data[['Todo']] <- Todo.Data
+
+storage <- configuration |> 
+  Storage::Mock.Storage.Broker(mock.data) |>
+  Storage::Storage.Service()
 
 # Data Layer
 data  <- storage |> Todo.Orchestration()
