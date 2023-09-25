be3ccdcc056e3fe4121d313810fd5a814063f299
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-25 00:59:31 -0700

    CODE RUB: Update Unit Tests

--- tests/testthat/test-Todo.Broker.R
+++ tests/testthat/test-Todo.Broker.R
@@ -54,6 +54,7 @@ describe("When todo |> operation[['Insert']]()",{
   it('then todo is inserted into storage',{
     # Given
     operation <- storage |> Todo.Broker()
+    Todo.Mock.Data |> storage[['Seed']]('Todo')
 
     new.todo      <- 'Task' |> Todo.Model()
     expected.todo <- new.todo
@@ -71,6 +72,7 @@ describe("When operation[['Select']]()",{
   it('then all todos are retrieved from storage',{
     # When
     operation <- storage |> Todo.Broker()
+    Todo.Mock.Data |> storage[['Seed']]('Todo')
 
     expected.todos <- storage[['Todo']][['Select']]()
 
@@ -85,6 +87,7 @@ describe("When id |> operation[['SelectById']]()",{
   it('then todo with matching id is retrieved from storage',{
     # When
     operation <- storage |> Todo.Broker()
+    Todo.Mock.Data |> storage[['Seed']]('Todo')
 
     existing.todo <- storage[['Todo']][['Select']]() |> tail(1)
 
@@ -102,6 +105,7 @@ describe("When todo |> operation[['Update']]()",{
   it('then todo is updated in storage',{
     # When
     operation <- storage |> Todo.Broker()
+    Todo.Mock.Data |> storage[['Seed']]('Todo')
 
     existing.todo <- storage[['Todo']][['Select']]() |> tail(1)
 
@@ -123,6 +127,7 @@ describe("When id |> operation[['Delete']]()",{
   it("then todo with matching id is deleted from storage",{
     # When
     operation <- storage |> Todo.Broker()
+    Todo.Mock.Data |> storage[['Seed']]('Todo')
 
     existing.todo <- storage[['Todo']][['Select']]() |> tail(1)
 
